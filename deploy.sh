2e06323ecc6e018372caeb4dbd141fd40dd944d7
John Dakey <johnsedofiadakey@gmail.com>
2026-07-24 18:53:56 +0000

    fix: deployment branch from master to main

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ set -e  # Exit on first error
 
 echo "Pulling latest code..."
 git config --global --add safe.directory "$(pwd)"  # Fix Git safe-dir issue in some CI
-git pull origin master
+git pull origin main
 
 echo "Starting containers..."
 sudo docker compose -f docker-compose.prod.yml --env-file .env.prod up -d --build
